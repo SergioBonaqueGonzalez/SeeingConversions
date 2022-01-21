d793f2b2c3f398eabd9724c287882d2c364b0a37
Sergio Bonaque-Gonzalez <sergio.bonaque.gonzalez@gmail.com>
2022-01-21 14:40:36 +0000

    Update coherenceTimeCalculation.m

--- coherenceTimeCalculation.m
+++ coherenceTimeCalculation.m
@@ -10,7 +10,7 @@ function coherenceTime =  coherenceTimeCalculation(cn2, v, wavelength)
 It is similar to: t0 = 0.314*r0/v, but using corrections of V_(5/3)
 Adapted from: M. J. Townson, O. J. D. Farley, G. Orban de Xivry, J. Osborn, and A. P. Reeves, "AOtools: a Python package for adaptive optics modelling and analysis," Opt. Express 27, 31316-31329 (2019)
 
-Nov, 2021. sergio.bonaque.gonzalez@gmail.com
+Jan, 2022. sergio.bonaque.gonzalez@gmail.com
 %}
 
 Jv = (cn2*(v^(5/3)));
